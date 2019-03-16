3ea3c7029bf2480ae551e2421564f8a571dc2976
PBHotdog <33501521+PBHotdog@users.noreply.github.com>
2019-03-16 11:06:19 -0500

    Added project files

---
+++ OFSProject-FileUpload/OFSProject-FileUpload/VBForm.Designer.vb
@@ -0,0 +1,83 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class VBForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.SystemColors.Control
+        Me.Button1.Location = New System.Drawing.Point(34, 259)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Select File"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(417, 259)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Submit File"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        Me.OpenFileDialog1.Filter = "*.csv|*.xls|*.xlsx|"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(87, 25)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(352, 218)
+        Me.DataGridView1.TabIndex = 2
+        '
+        'VBForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(531, 332)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "VBForm"
+        Me.Text = "File Select"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents DataGridView1 As DataGridView
+End Class
---
+++ OFSProject-FileUpload/OFSProject-FileUpload/VBForm.vb
@@ -0,0 +1,49 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports Amazon
+Imports Amazon.S3
+Imports Amazon.S3.Transfer
+Imports Amazon.S3.Model
+
+Public Class VBForm
+    Private Sub VBForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub ButtonClick(sender As Object, e As EventArgs) Handles Button1.Click, Button2.Click
+        Dim btn As Button
+        Try
+            btn = DirectCast(sender, Button)
+            Select Case btn.Name
+
+                Case Button1.Name
+
+                    Dim anothergreatvariable As String
+
+
+                    OpenFileDialog1.ShowDialog()
+
+                    anothergreatvariable = OpenFileDialog1.FileName
+                    Using wc As New System.Net.WebClient()
+                        wc.UploadFile("https://78cirdegtd.execute-api.us-east-1.amazonaws.com/default/textileEndpoint" & anothergreatvariable, anothergreatvariable)
+
+                    End Using
+
+
+                Case Button2.Name
+
+
+            End Select
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+
+
+        End Try
+
+
+
+    End Sub
+End Class
