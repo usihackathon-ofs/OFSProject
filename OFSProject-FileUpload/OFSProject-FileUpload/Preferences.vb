d86b4f5b4785603f75b941730ceb699d39da1dfe
mandyh92 <48624472+mandyh92@users.noreply.github.com>
2019-03-16 18:35:51 -0500

    Add files via upload

--- OFSProject-FileUpload/OFSProject-FileUpload/Preferences.vb
+++ OFSProject-FileUpload/OFSProject-FileUpload/Preferences.vb
@@ -2,10 +2,17 @@
     Private Sub Preferences_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         commonKey.Text = "Current Common Key: " + GetSetting("AWS", "Keys", "Common Key")
         secretKey.Text = "Current S.Key: " + GetSetting("AWS", "Keys", "Private Key")
+        lblBucketNameDesc.Text = "BucketName: " & GetSetting("AWS", "BucketName", "BucketName")
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If commonKeyTxt.Text = "" Then Exit Sub
+        If secretKeyTxt.Text = "" Then Exit Sub
+        If txtBucketName.Text = "" Then Exit Sub
+
         SaveSetting("AWS", "Keys", "Common Key", commonKeyTxt.Text)
         SaveSetting("AWS", "Keys", "Secret Key", secretKeyTxt.Text)
+        SaveSetting("AWS", "BucketName", "BucketName", txtBucketName.Text)
+
     End Sub
 End Class
